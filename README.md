e340aa3d92c0643ca00385e7bc0afa9a12c2f951
SJS <sjsevadjian@gmail.com>
2020-08-17 13:47:21 -0700

    trying to save modal form input into localStorage

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-## Acceptance Criteria
+## Work Day Scheduler
 
+A standard work day calendar contains timeblocks from 9am - 6pm.
+When the planner is opened, the current day is displayed at the top of the calendar.
+When you scroll down, the time blocks are color-coded as gray for time that has past, blue for time that is present, and faded blue/teal for time in the future.
+When I click 'Add' on a timeblock, a modal window opens for me to save an event.
+Event is saved to localStorage.
+When I refresh the page, any saved events persist.
 ```
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   const $currentDay = $("#currentDay");
+  const $eventSaver = $("#saveEvent");
+  const $save = $("#save");
   const $am9 = $("#am9");
   const $am10 = $("#am10");
   const $am11 = $("#am11");
@@ -10,7 +12,8 @@ $(document).ready(function () {
   const $pm4 = $("#pm4");
   const $pm5 = $("#pm5");
   const $pm6 = $("#pm6");
-
+  let events = [];
+  //localStorage.getItem("event");
   const m = moment();
   const dayOfWeek = (m.format(`dddd`) + ', ' + m.format('MMM Do'));
   const currentTime = m.format('h:mm');
@@ -28,7 +31,7 @@ $(document).ready(function () {
     else {
       $am9.addClass("alert-primary");
       if (minute < 9) {
-        $am9.addClass("alert-success");
+        $am9.addClass("alert-info");
       }
     }
     if (minute > 10) {
@@ -37,7 +40,7 @@ $(document).ready(function () {
     else {
       $am10.addClass("alert-primary");
       if (minute < 10) {
-        $am10.addClass("alert-success");
+        $am10.addClass("alert-info");
       }
     }
     if (minute > 11) {
@@ -46,7 +49,7 @@ $(document).ready(function () {
     else {
       $am11.addClass("alert-primary");
       if (minute < 11) {
-        $am11.addClass("alert-success");
+        $am11.addClass("alert-info");
       }
     }
     if (minute > 12) {
@@ -55,7 +58,7 @@ $(document).ready(function () {
     else {
       $pm12.addClass("alert-primary");
       if (minute < 12) {
-        $pm12.addClass("alert-success");
+        $pm12.addClass("alert-info");
       }
     }
     if (minute > 13) {
@@ -64,7 +67,7 @@ $(document).ready(function () {
     else {
       $pm1.addClass("alert-primary");
       if (minute < 13) {
-        $pm1.addClass("alert-success");
+        $pm1.addClass("alert-info");
       }
     }
     if (minute > 14) {
@@ -73,7 +76,7 @@ $(document).ready(function () {
     else {
       $pm2.addClass("alert-primary");
       if (minute < 14) {
-        $pm2.addClass("alert-success");
+        $pm2.addClass("alert-info");
       }
     }
     if (minute > 15) {
@@ -82,7 +85,7 @@ $(document).ready(function () {
     else {
       $pm3.addClass("alert-primary");
       if (minute < 15) {
-        $pm3.addClass("alert-success");
+        $pm3.addClass("alert-info");
       }
     }
     if (minute > 16) {
@@ -91,7 +94,7 @@ $(document).ready(function () {
     else {
       $pm4.addClass("alert-primary");
       if (minute < 16) {
-        $pm4.addClass("alert-success");
+        $pm4.addClass("alert-info");
       }
     }
     if (minute > 17) {
@@ -100,7 +103,7 @@ $(document).ready(function () {
     else {
       $pm5.addClass("alert-primary");
       if (minute < 17) {
-        $pm5.addClass("alert-success");
+        $pm5.addClass("alert-info");
       }
     }
     if (minute > 18) {
@@ -109,9 +112,14 @@ $(document).ready(function () {
     else {
       $pm6.addClass("alert-primary");
       if (minute < 18) {
-        $pm6.addClass("alert-success");
+        $pm6.addClass("alert-info");
       }
     }
   }
 
+  //Save to localStorage
+  $save.click(function() {
+    $pm6.textContent = events;
+    localStorage.setItem("events", JSON.stringify(events));
+  });
 });
