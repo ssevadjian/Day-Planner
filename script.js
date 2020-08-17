aca21a72c750c919374cc075950ff7ba4a61bbda
SJS <sjsevadjian@gmail.com>
2020-08-17 15:38:41 -0700

    updated index file with more descriptive ids; updated script file with ability to save to local storage, updated save button to close window when upon saving

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   const $currentDay = $("#currentDay");
-  const $eventSaver = $("#saveEvent");
   const $save = $("#save");
   const $am9 = $("#am9");
   const $am10 = $("#am10");
@@ -12,114 +11,119 @@ $(document).ready(function () {
   const $pm4 = $("#pm4");
   const $pm5 = $("#pm5");
   const $pm6 = $("#pm6");
-  let events = [];
-  //localStorage.getItem("event");
   const m = moment();
   const dayOfWeek = (m.format(`dddd`) + ', ' + m.format('MMM Do'));
-  const currentTime = m.format('h:mm');
-  const past = m.startOf('hour');
-  const minute = moment().hour();
-  console.log(minute);
+  const hour = moment().hour();
+  let events = JSON.parse(localStorage.getItem("myNotes")) || [];
+  //let eventNotes = JSON.parse(localStorage.getItem("myNotes")) || [];
+  console.log(events.myTitles);
+  //console.log(eventNotes);
 
+  // Displays today's date
   $currentDay.text(dayOfWeek);
-  //isPast function alerts dark if the time block is in the past, blue if it's current, and green if it's future 
+
+  // isPast function alerts dark if the time block is in the past, blue if it's current, and faded blue/teal if it's future 
   isPast();
   function isPast() {
-    if (minute > 9) {
+    if (hour > 9) {
       $am9.addClass("alert-dark");
     }
     else {
       $am9.addClass("alert-primary");
-      if (minute < 9) {
+      if (hour < 9) {
         $am9.addClass("alert-info");
       }
     }
-    if (minute > 10) {
+    if (hour > 10) {
       $am10.addClass("alert-dark");
     }
     else {
       $am10.addClass("alert-primary");
-      if (minute < 10) {
+      if (hour < 10) {
         $am10.addClass("alert-info");
       }
     }
-    if (minute > 11) {
+    if (hour > 11) {
       $am11.addClass("alert-dark");
     }
     else {
       $am11.addClass("alert-primary");
-      if (minute < 11) {
+      if (hour < 11) {
         $am11.addClass("alert-info");
       }
     }
-    if (minute > 12) {
+    if (hour > 12) {
       $pm12.addClass("alert-dark");
     }
     else {
       $pm12.addClass("alert-primary");
-      if (minute < 12) {
+      if (hour < 12) {
         $pm12.addClass("alert-info");
       }
     }
-    if (minute > 13) {
+    if (hour > 13) {
       $pm1.addClass("alert-dark");
     }
     else {
       $pm1.addClass("alert-primary");
-      if (minute < 13) {
+      if (hour < 13) {
         $pm1.addClass("alert-info");
       }
     }
-    if (minute > 14) {
+    if (hour > 14) {
       $pm2.addClass("alert-dark");
     }
     else {
       $pm2.addClass("alert-primary");
-      if (minute < 14) {
+      if (hour < 14) {
         $pm2.addClass("alert-info");
       }
     }
-    if (minute > 15) {
+    if (hour > 15) {
       $pm3.addClass("alert-dark");
     }
     else {
       $pm3.addClass("alert-primary");
-      if (minute < 15) {
+      if (hour < 15) {
         $pm3.addClass("alert-info");
       }
     }
-    if (minute > 16) {
+    if (hour > 16) {
       $pm4.addClass("alert-dark");
     }
     else {
       $pm4.addClass("alert-primary");
-      if (minute < 16) {
+      if (hour < 16) {
         $pm4.addClass("alert-info");
       }
     }
-    if (minute > 17) {
+    if (hour > 17) {
       $pm5.addClass("alert-dark");
     }
     else {
       $pm5.addClass("alert-primary");
-      if (minute < 17) {
+      if (hour < 17) {
         $pm5.addClass("alert-info");
       }
     }
-    if (minute > 18) {
+    if (hour > 18) {
       $pm6.addClass("alert-dark");
     }
     else {
       $pm6.addClass("alert-primary");
-      if (minute < 18) {
+      if (hour < 18) {
         $pm6.addClass("alert-info");
       }
     }
   }
+  
 
-  //Save to localStorage
+  //Save event form input to localStorage
   $save.click(function() {
-    $pm6.textContent = events;
-    localStorage.setItem("events", JSON.stringify(events));
+    const $titleInput = $("#titleInput").val();
+    const $notes = $("#notes").val();
+    events.push($notes);
+    localStorage.setItem("myNotes", JSON.stringify(events));
+    $('#eventModal').modal('hide');
   });
 });
