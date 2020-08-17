065bf88a9caabea4465e4d6c304a982bc72f6f7e
SJS <sjsevadjian@gmail.com>
2020-08-16 21:29:00 -0700

    updated html time blocks, added buttons & a modal; updated jQuery to respect time past, time present and future time

--- script.js
+++ script.js
@@ -1,11 +1,117 @@
-$( document ).ready(function() {
-    const getDayOfWeek = function (dayOfWeek) {
-        $.ajax({
-        url: "" + dayOfWeek + "&apikey=trilogy",
-        method: "GET"
-      }).then(function(response) {
-  
-       
-      });
+$(document).ready(function () {
+  const $currentDay = $("#currentDay");
+  const $am9 = $("#am9");
+  const $am10 = $("#am10");
+  const $am11 = $("#am11");
+  const $pm12 = $("#pm12");
+  const $pm1 = $("#pm1");
+  const $pm2 = $("#pm2");
+  const $pm3 = $("#pm3");
+  const $pm4 = $("#pm4");
+  const $pm5 = $("#pm5");
+  const $pm6 = $("#pm6");
+
+  const m = moment();
+  const dayOfWeek = (m.format(`dddd`) + ', ' + m.format('MMM Do'));
+  const currentTime = m.format('h:mm');
+  const past = m.startOf('hour');
+  const minute = moment().hour();
+  console.log(minute);
+
+  $currentDay.text(dayOfWeek);
+  //isPast function alerts dark if the time block is in the past, blue if it's current, and green if it's future 
+  isPast();
+  function isPast() {
+    if (minute > 9) {
+      $am9.addClass("alert-dark");
+    }
+    else {
+      $am9.addClass("alert-primary");
+      if (minute < 9) {
+        $am9.addClass("alert-success");
       }
+    }
+    if (minute > 10) {
+      $am10.addClass("alert-dark");
+    }
+    else {
+      $am10.addClass("alert-primary");
+      if (minute < 10) {
+        $am10.addClass("alert-success");
+      }
+    }
+    if (minute > 11) {
+      $am11.addClass("alert-dark");
+    }
+    else {
+      $am11.addClass("alert-primary");
+      if (minute < 11) {
+        $am11.addClass("alert-success");
+      }
+    }
+    if (minute > 12) {
+      $pm12.addClass("alert-dark");
+    }
+    else {
+      $pm12.addClass("alert-primary");
+      if (minute < 12) {
+        $pm12.addClass("alert-success");
+      }
+    }
+    if (minute > 13) {
+      $pm1.addClass("alert-dark");
+    }
+    else {
+      $pm1.addClass("alert-primary");
+      if (minute < 13) {
+        $pm1.addClass("alert-success");
+      }
+    }
+    if (minute > 14) {
+      $pm2.addClass("alert-dark");
+    }
+    else {
+      $pm2.addClass("alert-primary");
+      if (minute < 14) {
+        $pm2.addClass("alert-success");
+      }
+    }
+    if (minute > 15) {
+      $pm3.addClass("alert-dark");
+    }
+    else {
+      $pm3.addClass("alert-primary");
+      if (minute < 15) {
+        $pm3.addClass("alert-success");
+      }
+    }
+    if (minute > 16) {
+      $pm4.addClass("alert-dark");
+    }
+    else {
+      $pm4.addClass("alert-primary");
+      if (minute < 16) {
+        $pm4.addClass("alert-success");
+      }
+    }
+    if (minute > 17) {
+      $pm5.addClass("alert-dark");
+    }
+    else {
+      $pm5.addClass("alert-primary");
+      if (minute < 17) {
+        $pm5.addClass("alert-success");
+      }
+    }
+    if (minute > 18) {
+      $pm6.addClass("alert-dark");
+    }
+    else {
+      $pm6.addClass("alert-primary");
+      if (minute < 18) {
+        $pm6.addClass("alert-success");
+      }
+    }
+  }
+
 });
